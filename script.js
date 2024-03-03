2d074f525a8ec5eb935c42993e20c2ab7534f187
rajdipg000 <pradipg000@gmail.com>
2024-03-03 16:36:43 +0530

    File size reduce

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-$(document).ready(() => {
-    for (i = 0; i < 12; i++) {
-        var spoke = "<span class=spiral id='line" + i + "'></span>";
-        var deg = 15 * i;
-        $(".pos").append(spoke);
-        $("#line" + i + "").css("rotate", deg + "deg");
-    }
-});
+$(document).ready((()=>{for(i=0;i<12;i++){var a="<span class=spiral id='line"+i+"'></span>",s=15*i;$(".pos").append(a),$("#line"+i).css("rotate",s+"deg")}}));
