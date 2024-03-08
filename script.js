06fa5ade53872804cf096dc56893efe91016c059
rajdipg000 <pradipg000@gmail.com>
2024-03-08 12:36:13 +0530

    Nation

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-$(document).ready((()=>{for(i=0;i<12;i++){var a="<span class=spiral id='line"+i+"'></span>",s=15*i;$(".pos").append(a),$("#line"+i).css("rotate",s+"deg")}}));
+$(document).ready((() => { 
+    for (i = 0; i < 12; i++) {
+        var a = "<span class=spiral id='line" + i + "'></span>", s = 15 * i;
+        $(".pos").append(a), $("#line" + i).css("rotate", s + "deg")
+    }
+}));
